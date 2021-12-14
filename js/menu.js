961802ee9c665d6c9ce0babd9e875d8f2f2baebc
rodrigoas08 <95864934+rodrigoas08@users.noreply.github.com>
2021-12-14 17:52:45 -0300

    projeto final do curso
    projeto final

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,6 @@ document.querySelector('.menu-fechar').onclick = function() {
     document.documentElement.classList.remove('menu-ativo');
 };
 
-//opicional
-
 document.documentElement.onclick = function(event) {
     if (event.target === document.documentElement) {
         document.documentElement.classList.remove('menu-ativo');
